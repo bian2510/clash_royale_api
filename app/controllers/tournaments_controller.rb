adbe6a9d55f064fae513a035985aa9e4e0137297
Carlos Hernandez <bian251091@gmail.com>
2019-08-19 19:25:58 -0300

    show all tournament of a clan

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -4,6 +4,18 @@ class TournamentsController < ApplicationController
   end
   
   def show
-    render json: Tournament.find_by(clan_tag: '#VP08QJV')
+    render json: Tournament.where(clan_tag: "##{normalize_clan_tag}")
+  end
+
+  private
+
+  def normalize_clan_tag
+    clan_tag_up_case = params[:id].upcase
+    normalize_clan_tag = clan_tag_up_case.gsub('*', '') if clan_tag_up_case.include? '*'
+    normalize_clan_tag = clan_tag_upcase
+  end
+
+  def tournaments_params
+    params.require(:tournament).permit!(:clan_tag, :limit_time)
   end
 end
