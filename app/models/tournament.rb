92b59f906e7438743afa56d685f49eb905a706ac
Carlos Hernandez <bian251091@gmail.com>
2019-08-19 20:10:11 -0300

    add validation presence of params for create

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,2 +1,6 @@
 class Tournament < ApplicationRecord
+  validates :clan_tag, presence: true
+  validates :time_per_round, presence: true
+  validates :players, presence: true
+  validates :rounds, presence: true
 end
