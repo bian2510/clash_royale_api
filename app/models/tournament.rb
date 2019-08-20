e5a947a25dfcef854f448b08c115e3c24c3bdb04
Carlos Hernandez <bian251091@gmail.com>
2019-08-20 01:01:36 -0300

    validates type data is integer

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,6 +1,6 @@
 class Tournament < ApplicationRecord
   validates :clan_tag, presence: true
-  validates :time_per_round, presence: true
-  validates :players, presence: true
-  validates :rounds, presence: true
+  validates :time_per_round, presence: true, numericality: { only_integer: true }
+  validates :players, presence: true, numericality: { only_integer: true }
+  validates :rounds, presence: true, numericality: { only_integer: true }
 end
