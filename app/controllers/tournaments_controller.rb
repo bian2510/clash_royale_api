95cb2d015e8fd32e181d93c6a2fe190f88bd7177
fabian <bian251091@gmail.com>
2019-08-20 16:04:09 -0300

    fix create endpoint

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,39 +1,38 @@
 class TournamentsController < ApplicationController
+  before_action :validate_clan_exist, only: [:create]
   def index
     render json: Tournament.all
   end
   
   def show
-    render json: Tournament.where(clan_tag: "##{normalize_clan_tag}")
+    render json: Tournament.where(clan_tag: "##{normalize_clan_tag(params[:id])}")
   end
 
   def create
-    if validate_clan_exist != nil
-      tournament = Tournament.new(tournaments_params)
-      if tournament.save
-        render json: tournament, status: :created
-      else
-        # This line overrides the default rendering behavior, which
-        # would have been to render the "create" view.
-        render json: {error: "the tournament can't be created"}, status: :unprocessable_entity
-      end
+    tournament = Tournament.new(tournaments_params)
+    if tournament.save
+      render json: tournament, status: :created
+    else
+      # This line overrides the default rendering behavior, which
+      # would have been to render the "create" view.
+      render json: {error: "the tournament can't be created"}, status: :unprocessable_entity
     end
   end
 
   private
 
-  def normalize_clan_tag
-    clan_tag_up_case = params[:id].upcase
-    normalize_clan_tag = clan_tag_up_case.gsub('*', '') if clan_tag_up_case.include? '*'
-    normalize_clan_tag = clan_tag_up_case
+  def normalize_clan_tag(clan_tag)
+    normalized_clan_tag = clan_tag.upcase
   end
 
   def validate_clan_exist
-    token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjM5ODQ1ODhlLWFlZDMtNDZmYi1iZWZkLWNmYTFmNmMwMmI5ZCIsImlhdCI6MTU2NjA4MzgwMiwic3ViIjoiZGV2ZWxvcGVyL2ZlM2VmMzYzLTBmNmYtZGE4NS0yNmE0LTIxZTk3M2E5NjQ4ZiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxODEuNDYuMTM5LjExIl0sInR5cGUiOiJjbGllbnQifV19.NKx1O_aqZRea5_CrwrOZTo1p0HTLdG0eaDuR30UN9TwQhhg1M1Umxt7Za4S_dZhtSzzRmD08XZv3tcQ6Jwz-cA"
+    token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijg2MDM3MGIxLTdmNmMtNDhhYS1hNTY5LTFkYzBmZDNlMDNjMiIsImlhdCI6MTU2NjMyNzU0NCwic3ViIjoiZGV2ZWxvcGVyL2ZlM2VmMzYzLTBmNmYtZGE4NS0yNmE0LTIxZTk3M2E5NjQ4ZiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxODYuMTM5LjEwMy4xNTkiXSwidHlwZSI6ImNsaWVudCJ9XX0._zFDF4yRKar-ldxwtjI7jPaiUdgEO59-WduuzqqxqRX_l2FrhoQGknXnomCirvBmhGe-RtsiJtMCUegBFLlfqw"
     base_url = 'https://api.clashroyale.com/v1'
-    clan_tag = tournaments_params[:clan_tag].gsub('#','')
+    clan_tag = normalize_clan_tag(tournaments_params[:clan_tag])
     get_clan = "/clans/%23#{clan_tag}"
     response = HTTParty.get(base_url + get_clan, :headers => {"Authorization" => token})
+    return render json: {error: "Not found clan"}, status: :not_found if response.code == 404
+    JSON.parse(response.body)
   end
 
   def tournaments_params
