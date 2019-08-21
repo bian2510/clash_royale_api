46337e05afcc30bb3dafe129d763f86eac980f76
fabian <bian251091@gmail.com>
2019-08-21 14:45:45 -0300

    add url with BuilderUrl

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -27,12 +27,11 @@ class TournamentsController < ApplicationController
 
   def validate_clan_exist
     token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijg2MDM3MGIxLTdmNmMtNDhhYS1hNTY5LTFkYzBmZDNlMDNjMiIsImlhdCI6MTU2NjMyNzU0NCwic3ViIjoiZGV2ZWxvcGVyL2ZlM2VmMzYzLTBmNmYtZGE4NS0yNmE0LTIxZTk3M2E5NjQ4ZiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxODYuMTM5LjEwMy4xNTkiXSwidHlwZSI6ImNsaWVudCJ9XX0._zFDF4yRKar-ldxwtjI7jPaiUdgEO59-WduuzqqxqRX_l2FrhoQGknXnomCirvBmhGe-RtsiJtMCUegBFLlfqw"
-    base_url = 'https://api.clashroyale.com/v1'
     clan_tag = normalize_clan_tag(tournaments_params[:clan_tag])
-    path_url = "/clans/%23#{clan_tag}"
-    response = HTTParty.get(base_url + path_url, :headers => {"Authorization" => token})
+    url = BuilderUrl.new.get_clan(clan_tag)
+    response = HTTParty.get(url, :headers => {"Authorization" => token})
     return render json: {error: "Not found clan"}, status: :not_found if response.code == 404
-    JSON.parse(response.body)
+    JSON.parse(response.body) if response.code == 200
   end
 
   def tournaments_params
