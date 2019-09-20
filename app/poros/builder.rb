ab907a4e2d3df2af291c5ff86f2a1aa9c46918ad
fabian <bian251091@gmail.com>
2019-09-20 14:55:30 -0300

    fix rubocop

--- app/poros/builder.rb
+++ app/poros/builder.rb
@@ -3,11 +3,15 @@ class Builder
     @base_url = 'https://api.clashroyale.com/v1'
   end
 
+  def url_get_battle_log(player_tag)
+    @base_url + "/players/#{player_tag}/battlelog"
+  end
 
   def url_get_clan(clan_tag)
     @base_url + '/clans/%23' + clan_tag
   end
+
   def headers
-    {"Authorization": "Bearer #{ENV["API_KEY_SUPERCELL"]}"}
+    { "Authorization": "Bearer #{ENV['API_KEY_SUPERCELL']}" }
   end
 end
