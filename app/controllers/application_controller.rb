6c0805ecb26db56aeafb3144d55e0ef09411a93b
fabian <bian251091@gmail.com>
2019-09-20 14:53:22 -0300

    fix rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,20 @@
 class ApplicationController < ActionController::API
   def validation_for_create_tournaments
-    keys = %W[clan_tag time_per_round players rounds]
-    return render json: {error: 400}, status: :bad_request unless params[:clan_tag].present?
+    keys = %w[clan_tag time_per_round players rounds]
+    unless params[:clan_tag].present?
+      return render json: { error: 400 }, status: :bad_request
+    end
+
     validates_params(tournaments_params_create.to_h, keys)
     validate_clan_exist(tournaments_params_create[:clan_tag])
   end
 
   def validation_for_update_tournaments
-    keys = %W[first_place second_place best_streak unbeaten]
-    return render json: {error: 400}, status: :bad_request unless params[:id].present?
+    keys = %w[first_place second_place best_streak unbeaten]
+    unless params[:id].present?
+      return render json: { error: 400 }, status: :bad_request
+    end
+
     validates_params(tournaments_params_update.to_h, keys)
     validate_clan_exist(params[:id])
   end
@@ -16,21 +22,26 @@ class ApplicationController < ActionController::API
   private
 
   def validate_clan_exist(clan_tag)
-    response = HTTParty.get(Builder.new.url_get_clan(clan_tag), headers: Builder.new.headers)
-    render json: {error: "#{response.code}"}, status: :not_found if response.code != 200
+    response = HTTParty.get(Builder.new.url_get_clan(clan_tag),
+                            headers: Builder.new.headers)
+    if response.code != 200
+      render json: { error: response.code.to_s }, status: :not_found
+    end
     JSON.parse(response.body)
   end
 
   def validates_params(params_method, keys_expected)
     unless keys_expected.size == params_method.size
-      render json: {error: "bad_request"}, status: :bad_request
+      render json: { error: 'bad_request' }, status: :bad_request
     end
     validate_each_param(params_method, keys_expected)
   end
 
   def validate_each_param(parameters, keys)
-    parameters.each do |key, value|
-      render json: {error: "bad_request"}, status: :bad_request unless keys.include?(key)
+    parameters.each do |key, _value|
+      unless keys.include?(key)
+        render json: { error: 'bad_request' }, status: :bad_request
+      end
     end
   end
 end
--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -4,9 +4,9 @@ class TournamentsController < ApplicationController
   def index
     render json: Tournament.all
   end
-  
+
   def show
-    render json: Tournament.where(clan_tag: "#{params[:id]}")
+    render json: Tournament.where(clan_tag: params[:id].to_s)
   end
 
   def create
@@ -14,27 +14,30 @@ class TournamentsController < ApplicationController
     if tournament.save
       render json: tournament, status: :created
     else
-      render json: {error: "the tournament can't be created"}, status: :unprocessable_entity
+      render json: { error: "the tournament can't be created" }, 
+             status: :unprocessable_entity
     end
   end
 
   def update
-    tournament = Tournament.where(clan_tag: "#{params[:id]}").last
+    tournament = Tournament.where(clan_tag: params[:id].to_s).last
     if tournament.update(tournaments_params_update)
       render json: tournament, status: :ok
     else
-      render json: {error: "the tournament can't be edited"}, status: :unprocessable_entity
+      render json: { error: "the tournament can't be edited" },
+             status: :unprocessable_entity
     end
   end
 
   private
 
   def tournaments_params_create
-    params.require(:tournament).permit(:clan_tag, :time_per_round, :players, :rounds)
+    params.require(:tournament).permit(:clan_tag, :time_per_round, :players,
+                                       :rounds)
   end
 
   def tournaments_params_update
-    params.require(:tournament).permit(:first_place, :second_place, :best_streak, :unbeaten)
+    params.require(:tournament).permit(:first_place, :second_place, :best_streak,
+                                       :unbeaten)
   end
 end
-
