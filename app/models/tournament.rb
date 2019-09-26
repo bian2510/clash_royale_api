1d20660f2dd05286aa248f94fb42634b7463b75f
fabian <bian251091@gmail.com>
2019-09-26 18:13:52 -0300

    add relationship with round model

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,4 +1,5 @@
 class Tournament < ApplicationRecord
+  has_many :rounds
   validates :clan_tag, presence: true
   validates :time_per_round, presence: true, numericality: { only_integer: true }
   validates :players, presence: true, numericality: { only_integer: true }
