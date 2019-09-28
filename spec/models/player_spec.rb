2bbf8509c6016b9a381442f49b57e74d8fd5b952
Carlos Hernandez <bian251091@gmail.com>
2019-09-27 23:39:29 -0300

    add test of model player

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Create a player" do
+    it 'With name and tag' do
+      player = Player.new(name: "Name", tag: "TAG")
+      expect(player).to be_valid
+    end
+    it 'With name, tag, champion and unbeaten' do
+      player = Player.new(name: 'Name', tag: 'TAG', champion: true, unbeaten: true)
+      expect(player).to be_valid
+    end
+  end
+  
+  context "Can't create a player" do
+    it 'If tag is missing' do
+      player = Player.new(name: "Name", unbeaten: true, champion: false)
+      expect(player).to_not be_valid
+    end
+    it "If name is missing" do
+      player = Player.new(tag: "TAG", unbeaten: true, champion: false)
+      expect(player).to_not be_valid
+    end
+  end
 end
