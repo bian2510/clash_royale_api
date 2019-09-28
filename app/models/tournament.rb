0c0246b640d115262b112475d28bc9fc33c03988
Carlos Hernandez <bian251091@gmail.com>
2019-09-28 01:45:18 -0300

    add validate presence players

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -3,4 +3,5 @@ class Tournament < ApplicationRecord
   validates :clan_tag, presence: true
   validates :time_per_round, presence: true, numericality: { only_integer: true }
   validates :rounds, presence: true, numericality: { only_integer: true }
+  validates :players, presence: true
 end
