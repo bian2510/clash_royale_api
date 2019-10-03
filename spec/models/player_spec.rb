1eb39875bee57681a4eaf2cd656b0fa336d72094
Carlos Hernandez <bian251091@gmail.com>
2019-10-02 21:17:11 -0300

    refactor player_spec

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,15 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  context "Create a player" do
-    it 'with required fields' do
+  context "when creating a player with required fields" do
+    it 'it is valid' do
       player = Player.new(name: "Name", tag: "TAG")
       expect(player).to be_valid
     end
-  end
-  
-  context "Can't create a player" do
-    it 'If required fields is missing' do
+    it 'it is invalid' do
       player = Player.new(unbeaten: true, champion: false)
       expect(player).to_not be_valid
     end
